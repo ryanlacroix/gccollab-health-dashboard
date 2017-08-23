8943b736c4a191229d3ee234bf2f2339bb8a76e0
Ryan Lacroix <ryan.lacroix53@gmail.com>
2017-08-23 08:43:58 -0400

    Added more measures

---
+++ jobs/wireposthealth.rb
@@ -0,0 +1,7 @@
+require 'json'
+
+SCHEDULER.every '1h', :first_in => 0 do |job|
+    file = File.read(Dir.pwd + '/db_data/ind_health.json')
+    data_hash = JSON.parse(file)
+    send_event('wirepost-health',   { value: data_hash['wireposts'] })
+end
